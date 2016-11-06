be9312a9c97daf2437c21dc99a97c61438a5d7e6
Muhammad Hamza <muhammad00hamza@gmail.com>
2016-11-06 08:27:09 -0800

    Final year Project -First Module

--- app/src/main/java/com/example/hamza/finalyearproject/ComplainSubmision.java
+++ app/src/main/java/com/example/hamza/finalyearproject/ComplainSubmision.java
@@ -1,14 +1,24 @@
 package com.example.hamza.finalyearproject;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import static android.R.attr.data;
 
 public class ComplainSubmision extends AppCompatActivity {
+
+    private static final int IMAGE_REQUEST = 1888;
+
+
 
     String[] towns ={"Baldia Town","Bin Qasim Town","Kimari Town","Korangi Town","New karachi Town","North Nazimabad Town","Gadap Town","Gulshan Town",
             "Gulberg Town","Landhi town","Lyari town","Liaquatabad town","Orangi Town","Jamshed town","Malir town","Sadar Town",
@@ -35,6 +45,16 @@ public class ComplainSubmision extends AppCompatActivity {
         compTypes.setAdapter(complainTypesadapter);
         compTypes.setThreshold(1);
 
+        //Camera Intent
+        ImageView cameraIntent = (ImageView) findViewById(R.id.camera);
+        cameraIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,IMAGE_REQUEST );
+            }
+        });
+
         //NextButton Of Complainsubmision
         Button complainNextButton = (Button) findViewById(R.id.button_next);
         complainNextButton.setOnClickListener(new View.OnClickListener() {
@@ -47,5 +67,16 @@ public class ComplainSubmision extends AppCompatActivity {
 
 
 
+    }
+
+    //On camera Intent Result
+
+    protected void onActivityResult(int requestCode,int resultCode,Intent intent)
+    {
+        if(requestCode==IMAGE_REQUEST && resultCode == Activity.RESULT_OK)
+
+        {
+          //  http://stackoverflow.com/questions/5991319/capture-image-from-camera-and-display-in-activity
+        }
     }
 }
